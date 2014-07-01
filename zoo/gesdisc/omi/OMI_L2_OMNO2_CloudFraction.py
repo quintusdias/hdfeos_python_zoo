3379a414e8925c3585f534f015db2784088bbbe3
John Evans <john.g.evans.ne@gmail.com>
2014-07-01 07:34:39 -0400

    Added final NSIDC/MODIS swath example.  #1 Fixed OMI_L2_OMNO2_CloudFraction example to run with h5py. Introduced testing.

--- zoo/gesdisc/omi/OMI_L2_OMNO2_CloudFraction.py
+++ zoo/gesdisc/omi/OMI_L2_OMNO2_CloudFraction.py
@@ -13,34 +13,67 @@ import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-from netCDF4 import Dataset
 import numpy as np
+
+USE_NETCDF4 = True
 
 def run(FILE_NAME):
     DATAFIELD_NAME = 'CloudFraction'
     
-    dset = Dataset(FILE_NAME)
-    grp = dset.groups['HDFEOS'].groups['SWATHS'].groups['ColumnAmountNO2']
-    var = grp.groups['Data Fields'].variables[DATAFIELD_NAME]
-    data = var[:].astype(np.float64)
+    if USE_NETCDF4:
+
+        from netCDF4 import Dataset
+
+        dset = Dataset(FILE_NAME)
+        grp = dset.groups['HDFEOS'].groups['SWATHS'].groups['ColumnAmountNO2']
+        var = grp.groups['Data Fields'].variables[DATAFIELD_NAME]
+        
+        # netCDF4 doesn't quite handle the scaling correctly in this case since
+        # the attributes are "ScaleFactor" and "AddOffset" instead of
+        # "scale_factor" and "add_offset".  We'll do the scaling and conversion
+        # to a masked array ourselves.
+        var.set_auto_maskandscale(False)
+
+        data = var[:].astype(np.float64)
     
-    # Scale the data appropriatedy.
-    scale = var.ScaleFactor
-    offset = var.Offset
-    data = scale * (data - offset)
-    
-    # Replace the missing values with NaN.  Must create a masked array where
-    # NaN is involved.
-    missing_value = var.MissingValue
+        # Retrieve any attributes that may be needed later.
+        scale = var.ScaleFactor
+        offset = var.Offset
+        title = var.Title
+        missing_value = var.MissingValue
+        fill_value = var._FillValue
+
+        # Retrieve the geolocation data.
+        latitude = grp.groups['Geolocation Fields'].variables['Latitude'][:]
+        longitude = grp.groups['Geolocation Fields'].variables['Longitude'][:]
+
+    else:
+        
+        import h5py
+
+        path = '/HDFEOS/SWATHS/ColumnAmountNO2/Data Fields/'
+        DATAFIELD_NAME = path + 'CloudFraction'
+        with h5py.File(FILE_NAME, mode='r') as f:
+            dset = f[DATAFIELD_NAME]
+            data =dset[:].astype(np.float64)
+
+            # Retrieve any attributes that may be needed later.
+            scale = f[DATAFIELD_NAME].attrs['ScaleFactor']
+            offset = f[DATAFIELD_NAME].attrs['Offset']
+            missing_value = f[DATAFIELD_NAME].attrs['MissingValue']
+            fill_value = f[DATAFIELD_NAME].attrs['_FillValue']
+            title = f[DATAFIELD_NAME].attrs['Title']
+
+            # Retrieve the geolocation data.
+            path = '/HDFEOS/SWATHS/ColumnAmountNO2/Geolocation Fields/'
+            latitude = f[path + 'Latitude'][:]
+            longitude = f[path + 'Longitude'][:]
+
     data[data == missing_value] = np.nan
-    fill_value = var._FillValue
     data[data == fill_value] = np.nan
+    data = scale * (data - offset)
     datam = np.ma.masked_where(np.isnan(data), data)
-    
-    # Retrieve the geolocation data.
-    latitude = grp.groups['Geolocation Fields'].variables['Latitude'][:]
-    longitude = grp.groups['Geolocation Fields'].variables['Longitude'][:]
-    
+
     # Draw an equidistant cylindrical projection using the low resolution
     # coastline database.
     m = Basemap(projection='cyl', resolution='l',
@@ -53,27 +86,25 @@ def run(FILE_NAME):
     
     # Render the image in the projected coordinate system.
     x, y = m(longitude, latitude)
-    m.pcolormesh(x, y, datam, alpha=0.9)
+    m.pcolormesh(x, y, datam)
     m.colorbar()
     fig = plt.gcf()
     
-    plt.title('{0}\n{1})'.format(FILE_NAME, var.Title))
+    plt.title('{0})'.format(title))
     plt.show()
     
     filename = "{0}.{1}.png".format(os.path.basename(FILE_NAME)[:-4],
-                                    DATAFIELD_NAME)
+                                    os.path.basename(DATAFIELD_NAME))
     fig.savefig(filename)
 
 if __name__ == "__main__":
 
+    # If a certain environment variable is set, look there for the input
+    # file, otherwise look in the current directory.
+    hdffile = 'OMI-Aura_L2-OMNO2_2008m0720t2016-o21357_v003-2008m0721t101450.he5'
     try:
-        # If a certain environment variable is set, look there for the input
-        # file.
-        basename = 'OMI-Aura_L2-OMNO2_2008m0720t2016-o21357_v003-2008m0721t101450.he5'
-        fname = os.path.join(os.environ['HDFEOS_ZOO_DIR'], basename)
+        hdffile = os.path.join(os.environ['HDFEOS_ZOO_DIR'], hdffile)
     except KeyError:
-        # Look in the working directory.
-        fname = basename
+        pass
 
-    run(fname)
-    
+    run(hdffile)
