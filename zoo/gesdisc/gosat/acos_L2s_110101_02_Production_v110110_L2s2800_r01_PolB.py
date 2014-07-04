d15d2d4684c38a3a4c4f5a98de8012f164fb00df
John Evans <john.g.evans.ne@gmail.com>
2014-07-04 13:58:51 -0400

    Added GESDISC/GOSAT example.  Closes #17.

--- zoo/gesdisc/gosat/acos_L2s_110101_02_Production_v110110_L2s2800_r01_PolB.py
+++ zoo/gesdisc/gosat/acos_L2s_110101_02_Production_v110110_L2s2800_r01_PolB.py
@@ -45,27 +45,26 @@ def run(FILE_NAME):
         data_var = dset.groups['RetrievalResults'].variables['xco2']
         lat_var = dset.groups['SoundingGeometry'].variables['sounding_latitude_geoid']
         lon_var = dset.groups['SoundingGeometry'].variables['sounding_longitude_geoid']
+        lev_var = dset.groups['SoundingGeometry'].variables['sounding_altitude']
+        time_var = dset.groups['SoundingHeader'].variables['sounding_time_tai93']
 
         # Read the data.
-        #data = data_var[0,:,:]
+        data = data_var[:]
         lat = lat_var[:]
         lon = lon_var[:]
-        #lev = lev_var[:]
-        #date = date_var[0]
+        lev = lev_var[:]
+        time = time_var[:]
 
         # Read the needed attributes.
-        #data_units = data_var.units
-        #lat_units = lat_var.units
-        #lev_units = lev_var.units
-        #data_longname = data_var.long_name
-        #lat_longname = lat_var.long_name
-        #lev_longname = lev_var.long_name
+        data_units = data_var.Units
+        lev_units = lev_var.Units
 
     else:
         
         import h5py
 
         with h5py.File(FILE_NAME, mode='r') as f:
+
             dset_var = f['/RetrievalResults/xco2']
             dset_lat = f['/SoundingGeometry/sounding_latitude_geoid']
             dset_lon = f['/SoundingGeometry/sounding_longitude_geoid']
@@ -73,27 +72,20 @@ def run(FILE_NAME):
             dset_time = f['/SoundingHeader/sounding_time_tai93']
 
             # Read the data.
-            #data = dset_var[0,:,:]
+            data = dset_var[:]
             lat = dset_lat[:]
             lon = dset_lon[:]
-            #lev = dset_lev[:]
-            #date = dset_date[0]
+            lev = dset_lev[:]
+            time = dset_time[:]
 
             # Read the needed attributes.
-            #data_units = dset_var.attrs['units']
-            #lat_units = dset_lat.attrs['units']
-            #lev_units = dset_lev.attrs['units']
-            #data_longname = dset_var.attrs['long_name']
-            #lat_longname = dset_lat.attrs['long_name']
-            #lev_longname = dset_lev.attrs['long_name']
+            data_units = dset_var.attrs['Units'][0]
+            lev_units = dset_lev.attrs['Units'][0]
 
-            # H5PY doesn't automatically turn the data into a masked array.
-            #fillvalue = dset_var.attrs['_FillValue']
-            #data[data == fillvalue] = np.nan
-            #data = np.ma.masked_array(data, np.isnan(data))
-
-    # Draw an orthographic projection using the low resolution coastline
-    # database.
+    # First subplot is an orthographic projection using the low resolution
+    # coastline database.  Plot the trajectory.
+    fig = plt.figure(figsize=(15, 6))
+    plt.subplot(1, 2, 1)
     m = Basemap(projection='ortho', resolution='l', lat_0=-55, lon_0 = 120)
     m.drawcoastlines(linewidth=0.5)
     m.drawparallels(np.arange(-80., -0., 20.))
@@ -101,16 +93,49 @@ def run(FILE_NAME):
     x, y = m(lon, lat)
     m.plot(x, y)
 
-    # Annotate the starting point.
+    # Annotate the starting point.  Offset the annotation text by 200 km.
     m.plot(x[0], y[0], marker='o', color='red')
     plt.annotate('START',
-                 xy=(x[0] + 195000, y[0] + 5000),
+                 xy=(x[0] + 200000, y[0]),
                  xycoords='data',
                  color='red')
 
-    fig = plt.gcf()
-    
-    plt.title('Trajectory')
+    plt.title('GOSAT Trajectory')
+
+    # Second subplot will be time vs. CO2
+    ax1 = plt.subplot(1, 2, 2)
+    elapsed_time = time - time[0]
+    ax1.plot(elapsed_time, data, '-', color='black')
+    ax1.set_xlabel('Elapsed Time (seconds)')
+    ax1.set_ylabel('CO2 column averaged dry air mole fraction\n({0})'.format(data_units))
+
+    # Save some screen space by using scientific notation for the tick labels.
+    formatter = plt.ScalarFormatter(useMathText=True)
+    formatter.set_scientific(True)
+    formatter.set_powerlimits((-3, 4))
+    ax1.yaxis.set_major_formatter(formatter)
+
+    # Make a twin plot of time vs altitude.
+    # The color of the plot, ylabel, and ticks should be the same.
+    ax2 = ax1.twinx()
+    ax2.plot(elapsed_time, lev, '-', color='blue')
+    ax2.set_ylabel('Altitude ({0})'.format(lev_units), color='blue')
+    for tick in ax2.get_yticklabels():
+        tick.set_color('blue')
+
+    start_time = datetime.datetime(1993,1,1) + datetime.timedelta(seconds=time[0])
+    end_time = datetime.datetime(1993,1,1) + datetime.timedelta(seconds=time[-1])
+    titlestr = 'CO2 column averaged dry air mole fraction and Altitude\n'
+    titlestr += '{0} - {1}'.format(start_time.strftime('%Y-%m-%d %H:%M:%S'),
+                                   end_time.strftime('%Y-%m-%d %H:%M:%S'))
+
+    # Don't use "plt.title" in this case, as it will overlap with the ylabel
+    # text.  We need finer control as provided by the axis object methods.
+    ax2.text(0.5, 1.03, titlestr,
+             horizontalalignment='center',
+             fontsize=12,
+             transform=ax2.transAxes)
+
     plt.show()
     
     png = "{0}.{1}.png".format(os.path.basename(FILE_NAME)[:-4], 'trajectory')
