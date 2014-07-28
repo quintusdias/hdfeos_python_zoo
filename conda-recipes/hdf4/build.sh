9f89db1e63d4fe5fe99de40c389a94ee6e9c20b5
John Evans <john.g.evans.ne@gmail.com>
2014-07-28 18:38:43 -0400

    More similar to existing hdf4 build.sh.  Will most likely only work on x86_64.

--- conda-recipes/hdf4/build.sh
+++ conda-recipes/hdf4/build.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include $CFLAGS"
+mkdir -vp ${PREFIX}/bin;
+
+ARCH="$(uname 2>/dev/null)"
+
+#export CFLAGS="-I$PREFIX/include $CFLAGS"
+export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
+export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 
-./configure \
-    --enable-shared \
-    --disable-static \
-    --enable-fortran=no \
-    --disable-netcdf \
-    --prefix=$PREFIX
-make
-make check
-make install
+LinuxInstallation() {
 
-rm -rf $PREFIX/share
+    chmod +x configure;
+
+    ./configure \
+        --disable-netcdf \
+        --enable-fortran=no \
+        --disable-static \
+        --enable-linux-lfs \
+        --with-ssl \
+        --with-zlib \
+        --prefix=${PREFIX} || return 1;
+    make || return 1;
+    make check || return 1;
+    make install || return 1;
+
+    rm -rf ${PREFIX}/share/hdf4_examples;
+
+    return 0;
+}
+
+case ${ARCH} in
+    'Linux')
+        LinuxInstallation || exit 1;
+        ;;
+    *)
+        echo -e "Unsupported machine type: ${ARCH}";
+        exit 1;
+        ;;
+esac
+
