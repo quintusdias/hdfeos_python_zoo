2c8cf7beba10975e135d8a8f6fa5bc7b1c46fc30
jevans <john.g.evans.ne@gmail.com>
2014-07-28 22:10:48 -0400

    Works on 10.6.8.  Is DYLD_FALLBACK_LIBRARY_PATH ok to use or not???

--- conda-recipes/hdf4/build.sh
+++ conda-recipes/hdf4/build.sh
@@ -5,12 +5,35 @@ mkdir -vp ${PREFIX}/bin;
 ARCH="$(uname 2>/dev/null)"
 
 #export CFLAGS="-I$PREFIX/include $CFLAGS"
-export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 
+DarwinInstallation() {
+
+    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+
+    chmod +x configure;
+
+    ./configure \
+        --disable-netcdf \
+        --enable-fortran=no \
+        --with-jpeg=${PREFIX} \
+        --disable-static \
+        --without-szlib \
+        --with-ssl \
+        --with-zlib \
+        --prefix=${PREFIX} || return 1;
+    make || return 1;
+    make check || return 1;
+    make install || return 1;
+
+    rm -rf ${PREFIX}/share/hdf4_examples;
+
+    return 0;
+}
 LinuxInstallation() {
 
+    export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
     chmod +x configure;
 
     ./configure \
@@ -31,6 +54,9 @@ LinuxInstallation() {
 }
 
 case ${ARCH} in
+    'Darwin')
+        DarwinInstallation || exit 1;
+        ;;
     'Linux')
         LinuxInstallation || exit 1;
         ;;
