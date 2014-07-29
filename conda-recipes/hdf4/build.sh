bc2a08e5a386455b13bd919fdcae96149cc6da50
John Evans <john.g.evans.ne@gmail.com>
2014-07-29 18:54:25 -0400

    Don't need two separate functions for both mac and linux.

--- conda-recipes/hdf4/build.sh
+++ conda-recipes/hdf4/build.sh
@@ -4,65 +4,35 @@ mkdir -vp ${PREFIX}/bin;
 
 ARCH="$(uname 2>/dev/null)"
 
-#export CFLAGS="-I$PREFIX/include $CFLAGS"
-export CPPFLAGS="-I${PREFIX}/include"
-
-DarwinInstallation() {
-
-    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-	export DYLDFLAGS="-L$PREFIX/lib $LDFLAGS"
-
-    chmod +x configure;
-
-    ./configure \
-        --disable-netcdf \
-        --enable-fortran=no \
-        --with-jpeg=${PREFIX} \
-        --disable-static \
-        --without-szlib \
-        --with-ssl \
-        --with-zlib \
-        --prefix=${PREFIX} || return 1;
-    make || return 1;
-    make check || return 1;
-    make install || return 1;
-
-    rm -rf ${PREFIX}/share/hdf4_examples;
-
-    return 0;
-}
-LinuxInstallation() {
-
-    export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
-	export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-    chmod +x configure;
-
-    ./configure \
-        --disable-netcdf \
-        --enable-fortran=no \
-        --disable-static \
-        --enable-linux-lfs \
-        --with-ssl \
-        --with-zlib \
-        --prefix=${PREFIX} || return 1;
-    make || return 1;
-    make check || return 1;
-    make install || return 1;
-
-    rm -rf ${PREFIX}/share/hdf4_examples;
-
-    return 0;
-}
-
 case ${ARCH} in
     'Darwin')
-        DarwinInstallation || exit 1;
+        export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+    	export DYLDFLAGS="-L$PREFIX/lib $LDFLAGS"
         ;;
     'Linux')
-        LinuxInstallation || exit 1;
+	    export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
         ;;
     *)
         echo -e "Unsupported machine type: ${ARCH}";
         exit 1;
         ;;
 esac
+
+export CPPFLAGS="-I${PREFIX}/include"
+
+chmod +x configure;
+
+./configure \
+    --disable-netcdf \
+    --enable-fortran=no \
+    --with-jpeg=${PREFIX} \
+    --disable-static \
+    --with-zlib \
+    --prefix=${PREFIX} || return 1;
+make || exit 1;
+make check || exit 1;
+make install || exit 1;
+
+rm -rf ${PREFIX}/share/hdf4_examples;
+
+exit 0;
