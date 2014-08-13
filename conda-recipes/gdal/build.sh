4039522ab6d5d88d6f520815736e35814f93056d
John Evans <john.g.evans.ne@gmail.com>
2014-08-13 17:33:40 -0400

    Fixed gdal build script errors with hdf4 and hdf5.

--- conda-recipes/gdal/build.sh
+++ conda-recipes/gdal/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash configure --with-python --with-hdf=$PREFIX --with-hfd5=$PREFIX --prefix=$PREFIX
+bash configure --with-python --with-hdf4=$PREFIX --with-hdf5=$PREFIX --prefix=$PREFIX
 make
 make install
 
