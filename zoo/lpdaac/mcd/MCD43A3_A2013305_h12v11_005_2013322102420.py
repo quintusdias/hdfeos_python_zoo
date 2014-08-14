Fixed title strings to include units.

zoo/lpdaac/mcd/MCD43A3_A2013305_h12v11_005_2013322102420.py
@@ -44,6 +44,7 @@ def run(FILE_NAME):
 
     # Apply the attributes.
     meta = gdset.GetMetadata()
+    units = meta['units']
     fillvalue = np.float(meta['_FillValue'])
     scale = np.float(meta['scale_factor'])
     offset = np.float(meta['add_offset'])
@@ -79,7 +80,8 @@ def run(FILE_NAME):
     m.drawmeridians(np.arange(-70, -50, 5), labels=[0, 0, 0, 1])
     m.pcolormesh(lon[::2], lat[::2], data[::2])
     m.colorbar()
-    plt.title(DATAFIELD_NAME.replace('_', ' '))
+    title = "{0} ({1})".format(DATAFIELD_NAME.replace('_', ' '), units)
+    plt.title(title)
 
     fig = plt.gcf()
     plt.show()

zoo/lpdaac/mcd/MCD43B4_Nadir_Reflectance_Band1.py
@@ -44,6 +44,7 @@ def run(FILE_NAME):
 
     # Apply the attributes.
     meta = gdset.GetMetadata()
+    units = meta['units']
     fillvalue = np.float(meta['_FillValue'])
     scale = np.float(meta['scale_factor'])
     offset = np.float(meta['add_offset'])
@@ -75,7 +76,8 @@ def run(FILE_NAME):
     m.drawmeridians(np.arange(75, 115, 10), labels=[0, 0, 0, 1])
     m.pcolormesh(lon, lat, data)
     m.colorbar()
-    plt.title(DATAFIELD_NAME.replace('_', ' '))
+    title = "{0} ({1})".format(DATAFIELD_NAME.replace('_', ' '), units)
+    plt.title(title)
 
     fig = plt.gcf()
     plt.show()