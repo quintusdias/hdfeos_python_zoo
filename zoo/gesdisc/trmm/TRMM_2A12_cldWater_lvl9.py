914671404c123e0f009f4c633a99dbdce16d576b
H. Joe Lee <hyoklee@hdfgroup.org>
2014-09-10 11:59:13 -0500

    added PyHDF support and corrected unit label.

--- zoo/gesdisc/trmm/TRMM_2A12_cldWater_lvl9.py
+++ zoo/gesdisc/trmm/TRMM_2A12_cldWater_lvl9.py
@@ -1,4 +1,7 @@
 """
+Copyright (C) 2014 The HDF Group
+Copyright (C) 2014 John Evans
+
 This example code illustrates how to access and visualize a GESDISC TRMM file
 in Python.
 
@@ -24,32 +27,60 @@ import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-from netCDF4 import Dataset
 import numpy as np
+
+USE_NETCDF4 = False
 
 def run(FILE_NAME):
 
     DATAFIELD_NAME = 'cldWater'
-    
-    nc = Dataset(FILE_NAME)
-    var = nc.variables[DATAFIELD_NAME]
 
-    # cldWater has "scale_factor" and "add_offset" attributes, but the scaling
-    # equation to be used here is not 
+    if USE_NETCDF4:    
+        from netCDF4 import Dataset    
+        nc = Dataset(FILE_NAME)
+        var = nc.variables[DATAFIELD_NAME]
+
+        # Turn autoscaling off because this dataset does not follow the
+        # CF conventions scale/offset rule.
+        var.set_auto_maskandscale(False)
+
+        data = var[:,:,9].astype(np.double)
+        scale_factor = var.scale_factor
+        add_offset = var.add_offset
+
+        # Retrieve the geolocation data.
+        latitude = nc.variables['geolocation'][:,:,0]
+        longitude = nc.variables['geolocation'][:,:,1]
+    else:
+        from pyhdf.SD import SD, SDC
+        hdf = SD(FILE_NAME, SDC.READ)
+        
+        ds = hdf.select(DATAFIELD_NAME)
+        data = ds[:,:,9].astype(np.double)
+
+        # Handle scale/osffset attributes.
+        attrs = ds.attributes(full=1)
+        sfa=attrs["scale_factor"]
+        scale_factor = sfa[0]
+        aoa=attrs["add_offset"]
+        add_offset = aoa[0]
+
+        # Retrieve the geolocation data.        
+        geo = hdf.select('geolocation')
+        latitude = geo[:,:,0]
+        longitude = geo[:,:,1]
+
+    # The fill value is not explicitly set, but appears to be -9999.
+    data[data == -9999] = np.nan
+    # cldWater has "scale_factor" and "add_offset" attributes, 
+    # but the scaling equation to be used here does not follow the CF 
+    # conventoins
     #
     #     data = data * scale + offset
     #
-    # We'll turn autoscaling off in order to correctly scale the data.  Also,
-    # the fill value is not explicitly set, but appears to be -9999.
-    var.set_auto_maskandscale(False)
-    data = var[:,:,9].astype(np.double)
-    data[data == -9999] = np.nan
-    data = data / var.scale_factor + var.add_offset
+    data = data / scale_factor + add_offset
     datam = np.ma.masked_array(data, np.isnan(data))
     
-    # Retrieve the geolocation data.
-    latitude = nc.variables['geolocation'][:,:,0]
-    longitude = nc.variables['geolocation'][:,:,1]
     
     # There is a wrap-around effect to deal with.  Adjust the longitude by
     # modulus 360 to avoid the swath being smeared.
@@ -65,14 +96,14 @@ def run(FILE_NAME):
     m.drawparallels(np.arange(-90., 120., 30.), labels=[1, 0, 0, 0])
     m.drawmeridians(np.arange(-45, 315., 45.), labels=[0, 0, 0, 1])
     m.pcolormesh(longitude, latitude, datam, latlon=True, vmin=0, vmax=0.192)
-    m.colorbar()
-    plt.title('{0} (g/m^3)'.format(DATAFIELD_NAME))
+    cb = m.colorbar()
+    cb.set_label('Unit:g/m^-3')
 
+    basename = os.path.basename(FILE_NAME)
+    plt.title('{0}\n {1}'.format(basename, DATAFIELD_NAME))
     fig = plt.gcf()
-    plt.show()
-    
-    basename = os.path.splitext(os.path.basename(FILE_NAME))[0]
-    pngfile = "{0}.{1}.png".format(basename, DATAFIELD_NAME)
+    # plt.show()
+    pngfile = "{0}.py.png".format(basename)
     fig.savefig(pngfile)
 
 
