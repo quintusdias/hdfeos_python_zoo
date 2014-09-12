7686083a189e589c81236882ed9b08867de39a06
H. Joe Lee <hyoklee@hdfgroup.org>
2014-09-12 12:12:11 -0500

    added PyHDF support. fixed missing value handling.

--- zoo/gesdisc/toms/TOMS_L3_Ozone.py
+++ zoo/gesdisc/toms/TOMS_L3_Ozone.py
@@ -1,4 +1,7 @@
 """
+Copyright (C) 2014 The HDF Group
+Copyright (C) 2014 John Evans
+
 This example code illustrates how to access and visualize a GESDISC TOMS grid
 in Python.
 
@@ -25,19 +28,51 @@ import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-from netCDF4 import Dataset
 import numpy as np
+
+USE_NETCDF4 = False
 
 def run(FILE_NAME):
 
     DATAFIELD_NAME = 'Ozone'
-    
-    nc = Dataset(FILE_NAME)
-    data = nc.variables[DATAFIELD_NAME][:]
-    units = nc.variables[DATAFIELD_NAME].units
-    
-    latitude = nc.variables['YDim:TOMS Level 3'][:]
-    longitude = nc.variables['XDim:TOMS Level 3'][:]
+
+    if USE_NETCDF4:
+        from netCDF4 import Dataset    
+        nc = Dataset(FILE_NAME)
+        data = nc.variables[DATAFIELD_NAME][:]
+        missing_value = nc.variables[DATAFIELD_NAME].missing_value
+        units = nc.variables[DATAFIELD_NAME].units
+        long_name = nc.variables[DATAFIELD_NAME].long_name
+
+        latitude = nc.variables['YDim:TOMS Level 3'][:]
+        longitude = nc.variables['XDim:TOMS Level 3'][:]
+    else:
+        from pyhdf.SD import SD, SDC
+        hdf = SD(FILE_NAME, SDC.READ)
+
+        # Read dataset.
+        data2D = hdf.select(DATAFIELD_NAME)
+        data = data2D[:,:]
+
+        # Retrieve the attributes.
+        attrs = data2D.attributes(full=1)
+        mva=attrs["missing_value"]
+        missing_value = mva[0]
+        lna=attrs["long_name"]
+        long_name = lna[0]
+        ua=attrs["units"]
+        units = ua[0]        
+
+        # Read geolocation dataset.
+        lat = hdf.select('YDim:TOMS Level 3')
+        latitude = lat[:]
+        lon = hdf.select('XDim:TOMS Level 3')
+        longitude = lon[:]
+
+    # Replace the missing values with NaN.        
+    data[data == missing_value] = np.nan
+    datam = np.ma.masked_array(data, np.isnan(data))
+
     
     # Draw an equidistant cylindrical projection using the low resolution
     # coastline database.
@@ -47,15 +82,17 @@ def run(FILE_NAME):
     m.drawcoastlines(linewidth=0.5)
     m.drawparallels(np.arange(-90., 120., 30.), labels=[1, 0, 0, 0])
     m.drawmeridians(np.arange(-180, 180., 45.), labels=[0, 0, 0, 1])
-    m.pcolormesh(longitude, latitude, data, latlon=True)
-    m.colorbar()
-    plt.title('{0} ({1})'.format(DATAFIELD_NAME, units))
+    m.pcolormesh(longitude, latitude, datam, latlon=True)
+    cb = m.colorbar()
+    cb.set_label(units)
+
+    basename = os.path.basename(FILE_NAME)
+    plt.title('{0}\n{1}'.format(basename, long_name))
 
     fig = plt.gcf()
-    plt.show()
+    # plt.show()
     
-    basename = os.path.splitext(os.path.basename(FILE_NAME))[0]
-    pngfile = "{0}.{1}.png".format(basename, DATAFIELD_NAME)
+    pngfile = "{0}.py.png".format(basename)
     fig.savefig(pngfile)
 
 
