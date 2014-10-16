925c5f664d95501e71019ab998b5ca77a0061db5
H. Joe Lee <hyoklee@hdfgroup.org>
2014-10-16 15:58:57 -0500

    corrected output plot name for consistency.

--- zoo/larc/misr/MISR_AM1_TC_ALBEDO_l50.py
+++ zoo/larc/misr/MISR_AM1_TC_ALBEDO_l50.py
@@ -84,7 +84,7 @@ def run(FILE_NAME):
     plt.title('{0}\n{1} at SOMBlockDim=50 NBandDim=0'.format(basename, DATAFIELD_NAME))
     fig = plt.gcf()
     # plt.show()
-    pngfile = "{0}.l50.png".format(basename)
+    pngfile = "{0}.l50.py.png".format(basename)
     fig.savefig(pngfile)
 
 
