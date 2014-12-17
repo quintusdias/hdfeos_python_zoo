f10065c6cb666f7998d0cefddf7f8c8bf2e05bfd
jevans <john.g.evans.ne@gmail.com>
2014-12-16 20:41:13 -0500

    added pyhdfeos support

--- zoo/nsidc/amsre/AMSR_E_L3_DL_A_TB36_5H_Res_1.py
+++ zoo/nsidc/amsre/AMSR_E_L3_DL_A_TB36_5H_Res_1.py
@@ -27,42 +27,63 @@ from mpl_toolkits.basemap import Basemap
 import mpl_toolkits.basemap.pyproj as pyproj
 import numpy as np
 
+USE_PYHDFEOS = True
+
 def run(FILE_NAME):
     
     # Identify the data field.
-    GRID_NAME = 'Ascending_Land_Grid'
-    DATAFIELD_NAME = 'A_TB36.5H (Res 1)'
+    GRIDNAME = 'Ascending_Land_Grid'
+    FIELDNAME = 'A_TB36.5H (Res 1)'
+
+    if USE_PYHDFEOS:
+
+        from pyhdfeos import GridFile
+
+        gdf = GridFile(FILE_NAME)
+        data = gdf.grids[GRIDNAME].fields[FIELDNAME][:].astype(np.float64)
+        fillvalue = gdf.grids[GRIDNAME].fields[FIELDNAME].attrs['_FillValue']
+        data[data == fillvalue] = np.nan
+        data *= 0.1
+        data = np.ma.masked_array(data, np.isnan(data))
+
+        lat, lon = gdf.grids[GRIDNAME][:]
+
+    else:
+
+        import gdal
     
-    gname = 'HDF4_EOS:EOS_GRID:"{0}":{1}:{2}'.format(FILE_NAME,
-                                                     GRID_NAME,
-                                                     DATAFIELD_NAME)
-    gdset = gdal.Open(gname)
-    data = gdset.ReadAsArray().astype(np.float64)
+        gname = 'HDF4_EOS:EOS_GRID:"{0}":{1}:{2}'.format(FILE_NAME,
+                                                         GRIDNAME,
+                                                         FIELDNAME)
+        gdset = gdal.Open(gname)
+        data = gdset.ReadAsArray().astype(np.float64)
+    
+        # Apply the attributes information.
+        # Ref:  http://nsidc.org/data/docs/daac/ae_land3_l3_soil_moisture/data.html
+        meta = gdset.GetMetadata()
+        data[data == float(meta['_FillValue'])] = np.nan
+        data *= 0.1
+        data = np.ma.masked_array(data, np.isnan(data))
+    
+        # Construct the grid.  Reproject out of the global GCTP CEA into lat/lon.
+        # Ref:  http://nsidc.org/data/atlas/epsg_3410.html
+        meta = gdset.GetMetadata()
+        x0, xinc, _, y0, _, yinc = gdset.GetGeoTransform()
+        nx, ny = (gdset.RasterXSize, gdset.RasterYSize)
+        x = np.linspace(x0, x0 + xinc*nx, nx)
+        y = np.linspace(y0, y0 + yinc*ny, ny)
+        xv, yv = np.meshgrid(x, y)
+        args = ["+proj=cea",
+                "+lat_0=0",
+                "+lon_0=0",
+                "+lat_ts=30",
+                "+a=6371228",
+                "+units=m"]
+        pstereo = pyproj.Proj(' '.join(args))
+        wgs84 = pyproj.Proj("+init=EPSG:4326") 
+        lon, lat= pyproj.transform(pstereo, wgs84, xv, yv)
 
-    # Apply the attributes information.
-    # Ref:  http://nsidc.org/data/docs/daac/ae_land3_l3_soil_moisture/data.html
-    meta = gdset.GetMetadata()
-    data[data == float(meta['_FillValue'])] = np.nan
-    data *= 0.1
-    data = np.ma.masked_array(data, np.isnan(data))
-
-    # Construct the grid.  Reproject out of the global GCTP CEA into lat/lon.
-    # Ref:  http://nsidc.org/data/atlas/epsg_3410.html
-    meta = gdset.GetMetadata()
-    x0, xinc, _, y0, _, yinc = gdset.GetGeoTransform()
-    nx, ny = (gdset.RasterXSize, gdset.RasterYSize)
-    x = np.linspace(x0, x0 + xinc*nx, nx)
-    y = np.linspace(y0, y0 + yinc*ny, ny)
-    xv, yv = np.meshgrid(x, y)
-    args = ["+proj=cea",
-            "+lat_0=0",
-            "+lon_0=0",
-            "+lat_ts=30",
-            "+a=6371228",
-            "+units=m"]
-    pstereo = pyproj.Proj(' '.join(args))
-    wgs84 = pyproj.Proj("+init=EPSG:4326") 
-    lon, lat= pyproj.transform(pstereo, wgs84, xv, yv)
+        del gdset
 
     m = Basemap(projection='cyl', resolution='l',
                 llcrnrlat=-90, llcrnrlon=-180, urcrnrlat=90, urcrnrlon=180)
@@ -71,19 +92,16 @@ def run(FILE_NAME):
     m.drawmeridians(np.arange(-180, 181, 45), labels=[0, 0, 0, 1])
     m.pcolormesh(lon, lat, data, latlon=True)
     m.colorbar()
-    titlestr = '{0} (Kelvin)'.format(DATAFIELD_NAME.replace('_', ' '))
+    titlestr = '{0} (Kelvin)'.format(FIELDNAME.replace('_', ' '))
     plt.title(titlestr)
 
     fig = plt.gcf()
     plt.show()
     
     basename = os.path.splitext(os.path.basename(FILE_NAME))[0]
-    dfname = DATAFIELD_NAME.replace(' ','_').replace('(','_').replace(')','_')
+    dfname = FIELDNAME.replace(' ','_').replace('(','_').replace(')','_')
     pngfile = "{0}.{1}.png".format(basename, dfname)
     fig.savefig(pngfile)
-
-    del gdset
-
 
 if __name__ == "__main__":
 
