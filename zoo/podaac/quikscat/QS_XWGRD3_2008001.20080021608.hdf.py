146edb6bf9714151a7a19d0edf0db9f5943cdd09
H. Joe Lee <hyoklee@hdfgroup.org>
2015-06-05 16:24:42 -0500

    updated po.daac QuikScat plot.

--- zoo/podaac/quikscat/QS_XWGRD3_2008001.20080021608.hdf.py
+++ zoo/podaac/quikscat/QS_XWGRD3_2008001.20080021608.hdf.py
@@ -1,4 +1,7 @@
 """
+Copyright (C) 2015 The HDF Group
+Copyright (C) 2014 John Evans
+
 This example code illustrates how to access and visualize a PO.DAAC QuikSCAT
 grid file in Python.
 
@@ -11,13 +14,15 @@ contact us at eoshelp@hdfgroup.org or post it at the HDF-EOS Forum
 
 Usage:  save this script and run
 
-    python QS_XWGRDS_des_avg_wind_speed.py
+    python QS_XWGRD3_2008001.20080021608.hdf.py
 
 The HDF file must either be in your current working directory or in a directory
 specified by the environment variable HDFEOS_ZOO_DIR.
 
 The netcdf library must be compiled with HDF4 support in order for this example
 code to work.  Please see the README for details.
+
+Last Update: 2015/06/05
 """
 
 import os
@@ -25,26 +30,48 @@ import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-from netCDF4 import Dataset
 import numpy as np
+
+USE_NETCDF4 = False
 
 def run(FILE_NAME):
 
-    nc = Dataset(FILE_NAME)
-
     # Identify the data field.
     DATAFIELD_NAME = 'des_avg_wind_speed'
-    
-    # Subset the data to match the size of the swath geolocation fields.
-    # Turn off autoscaling, as the fillvalue attribute is not set.
-    var = nc.variables[DATAFIELD_NAME]
-    var.set_auto_maskandscale(False)
+
+    if USE_NETCDF4:
+        from netCDF4 import Dataset
+        nc = Dataset(FILE_NAME)
+        # Subset the data to match the size of the swath geolocation fields.
+        # Turn off autoscaling, as the fillvalue attribute is not set.
+        var = nc.variables[DATAFIELD_NAME]
+        var.set_auto_maskandscale(False)
+        scale = var.scale_factor
+        offset = var.add_offset
+        long_name = var.long_name
+        units = var.units
+    else:
+        from pyhdf.SD import SD, SDC
+        hdf = SD(FILE_NAME, SDC.READ)
+
+        # Read dataset.
+        var = hdf.select(DATAFIELD_NAME)
+
+        # Retrieve attributes.
+        attrs = var.attributes(full=1)
+        sfa=attrs["scale_factor"]
+        scale = sfa[0]   
+        aoa=attrs["add_offset"]
+        offset = aoa[0]
+        ua=attrs["units"]
+        units = ua[0]
+        lna=attrs["long_name"]
+        long_name = lna[0]
+
     data = var[:]
 
     # Retrieve the needed attributes.  By inspection, the fill value is 0.
     fillvalue = 0
-    scale = var.scale_factor
-    offset = var.add_offset
 
     invalid = data == fillvalue
     data = data * scale + offset
@@ -69,15 +96,17 @@ def run(FILE_NAME):
     m.drawparallels(np.arange(-90, 91, 45))
     m.drawmeridians(np.arange(-180, 180, 45), labels=[True,False,False,True])
     m.pcolormesh(longitude, latitude, datam, latlon=True)
-    m.colorbar()
-    plt.title('{0} ({1})\n'.format(DATAFIELD_NAME, var.units))
-    
+
+    cax = plt.axes([0.92, 0.3, 0.01, 0.4])
+    cb = plt.colorbar(cax=cax)
+    cb.set_label(units)    
+
+    basename = os.path.basename(FILE_NAME)
     fig = plt.gcf()
-    plt.show()
-    
-    basename = os.path.splitext(os.path.basename(FILE_NAME))[0]
-    pngfile = basename + ".png"
+    fig.suptitle('{0}\n{1}'.format(basename, long_name))
+    pngfile = "{0}.py.png".format(basename)
     fig.savefig(pngfile)
+
     
 if __name__ == "__main__":
 
